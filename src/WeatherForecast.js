0052c0bd257a3f34e9412f9739fb3bb045c7c91e
R.Benatti <rosanabenatti@gmail.com>
2024-10-14 18:29:58 +0200

    Footer styled.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,35 +2,21 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 
-export default function WeatherForecast(props) {
-  const forecastData = props.forecast;
-
-  if (!forecastData || forecastData.length === 0) {
-    return <p>Loading forecast...</p>; // Handle the case where forecast data is not available
-  }
-
-  // Slice the array to skip today's forecast
-  const dailyForecast = forecastData.slice(1, 8); // Next 7 days
+export default function WeatherForecast({ forecast }) {
+  const dailyForecast = forecast.slice(1, 8);
 
   return (
     <div className="forecast-container">
       <div className="forecast-grid">
         {dailyForecast.map((day, index) => (
           <div key={index} className="forecast-item">
-            {/* Day of the Week */}
             <p className="forecast-day">
               {new Date(day.dt * 1000).toLocaleDateString("en-US", {
                 weekday: "short",
               })}
             </p>
-
-            {/* Weather Icon */}
             <WeatherIcon code={day.weather[0].icon} size={36} />
-
-            {/* Max Temperature */}
             <p className="forecast-max-temp">{Math.round(day.temp.max)}°C</p>
-
-            {/* Min Temperature */}
             <p className="forecast-min-temp">{Math.round(day.temp.min)}°C</p>
           </div>
         ))}
